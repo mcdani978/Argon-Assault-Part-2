112c61fb0ff39a53190d1a44456ef9822be5e7d6
mcdani978 <mcdani978@morrisville.edu>
2024-10-27 22:42:42 -0400

    Player movement controlled using Unity’s Input System

--- Argon Assault Part 2/Assets/Scripts/PlayerControls.cs
+++ Argon Assault Part 2/Assets/Scripts/PlayerControls.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
-{
+{ 
+
+    [SerializeField] InputAction movement;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void OnEnable()
+    {
+        movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        float horizontalthrow = Input.GetAxis("Horizontal");
+        float horizontalthrow = movement.ReadValue<Vector2>().x;
+        float verticalthrow = movement.ReadValue<Vector2>().y;
+
+        //float horizontalthrow = Input.GetAxis("Horizontal");
         Debug.Log(horizontalthrow);
 
-        float verticalthrow = Input.GetAxis("Vertical");
+        //float verticalthrow = Input.GetAxis("Vertical");
         Debug.Log(verticalthrow);
     }
 }
