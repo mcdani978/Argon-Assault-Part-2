ccbd8ee3bdb01ab4bb0de041a218bee015266038
mcdani978 <mcdani978@morrisville.edu>
2024-10-28 00:02:12 -0400

    Final player movement implemented with offsets

--- Argon Assault Part 2/Assets/Scripts/PlayerControls.cs
+++ Argon Assault Part 2/Assets/Scripts/PlayerControls.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
-    [SerializeField] float moveSpeed = 10f; // Added speed multiplier
+    [SerializeField] float moveSpeed = 10f; 
 
     // Start is called before the first frame update
     void Start()
@@ -24,14 +24,18 @@ public class PlayerControls : MonoBehaviour
         movement.Disable();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         float xThrow = movement.ReadValue<Vector2>().x;
+        float yThrow = movement.ReadValue<Vector2>().y; 
 
         float xOffset = xThrow * moveSpeed * Time.deltaTime;
-        float newPosX = transform.localPosition.x + xOffset;
+        float yOffset = yThrow * moveSpeed * Time.deltaTime; 
 
-        transform.localPosition = new Vector3(newPosX, transform.localPosition.y, transform.localPosition.z);
+        float newPosX = transform.localPosition.x + xOffset;
+        float newPosY = transform.localPosition.y + yOffset; 
+
+        transform.localPosition = new Vector3(newPosX, newPosY, transform.localPosition.z);
     }
 }
